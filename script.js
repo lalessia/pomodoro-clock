5108d510288ee494f7cda9b750b9634917bafa05
lalessia <alessia.profeta@gmail.com>
2019-08-14 12:10:46 +0200

    improve timer management

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ let seconds;
 let minute;
 let workTimer = 1;
 let isNextBreakTimer = true;
+let isFirstSecond = true;
+
+function setClock(min, sec){
+    $("#countdown").text(min);
+    $(".point").css("animation-duration", sec);
+}
 
 $(document).ready(function() {
   $('input').on('keydown', function(e) {
@@ -10,11 +16,8 @@ $(document).ready(function() {
   });
   $("#pauseButton").prop("disabled", true);
   $("#resetButton").prop("disabled", true);
-  // $("#countdown").text("25:00");
-  // $(".point").css("animation-duration", "1500s");
 
-  $("#countdown").text("1:00");
-  $(".point").css("animation-duration", "60s");
+  setClock("25:00", "1500s");
 });
 
 function setDisabledButton(start, pause, reset){
@@ -24,68 +27,56 @@ function setDisabledButton(start, pause, reset){
 }
 
 function start(){
-    timer = setInterval(timeManager, 1000);
-
+    timer = setInterval(secondsManager, 1000);
     setDisabledButton(true, false, false);
-    $(".point").css("animation-play-state", "running");
 }
 
 function pause(){
-
     clearInterval(timer);
     $(".point").css("animation-play-state", "paused");
-
+    isFirstSecond = true;
     setDisabledButton(false, true, false);
 }
 
 function reset(){
     clearInterval(timer);
+    setClock("25:00", "1500s");
     $(".point").css("animation", "inner-circle linear infinite paused");
-    $("#countdown").text("25:00");
+    //$("#countdown").text("25:00");
+
     workTimer = 1;
     setDisabledButton(false, true, true);
-}
-
-function timeManager(){
-    secondsManager();
-    $("#countdown").text(minutes + ":" + seconds);
 }
 
 function breakManager(){
     if(workTimer % 4 == 0 && isNextBreakTimer){
         workTimer = 0;
-        // $("#countdown").text(30 + ":" + 00);
+        // $("#countdown").text("30:00");
         // $(".point").css("animation-duration", "1800s");
-        clearInterval(timer);
-        $("#countdown").text("3:00");
-        $(".point").css("animation-duration", "180s");
-
+        setClock("30:00", "1800s");
 
         isNextBreakTimer = false;
     } else if(isNextBreakTimer){
         workTimer++;
-        // $("#countdown").text(5 + ":" + 00);
-        // $(".point").css("animation-duration", "300s");
-        clearInterval(timer);
-        $("#countdown").text("0:30");
-        $(".point").css("animation-duration", "30s");
-
+        setClock("5:00", "300s");
         isNextBreakTimer = false;
     } else{
-        // $("#countdown").text(25 + ":" + 00);
-        // $(".point").css("animation-duration", "1500s");
-        clearInterval(timer);
-
-        $("#countdown").text("1:00");
-        $(".point").css("animation-duration", "60s");
-        $(".point").css("animation", "inner-circle linear infinite paused");
         isNextBreakTimer = true;
+        setClock("25:00", "1500s");
     }
-    console.log("isNextBreakTimer: " + isNextBreakTimer);
-    console.log("workTimer: " + workTimer);
-    timer = setInterval(timeManager, 1000);
-    $(".point").css("animation-play-state", "running");
-    secondsManager();
+    start();
+}
+
+function finishedRound(){
+
+    for(var i = 0; i < 5; i++){
+        setTimeout(
+            function(){
+                $('.blink').fadeOut(500);
+                $('.blink').fadeIn(500);
+            }, 1000);
+    }
+    breakManager();
 }
 
 function decrementMinutes(time){
@@ -95,23 +86,29 @@ function decrementMinutes(time){
 
 function secondsManager(){
     let timestr = $("#countdown").text();
-    console.log(timestr);
     time = timestr.split(":");
 
     seconds = time[1];
     minutes = time[0];
 
+    if(isFirstSecond){
+        $(".point").css("animation-play-state", "running");
+        isFirstSecond = false;
+    }
     if(seconds === '00' && minutes != '0'){
         decrementMinutes(time);
         seconds = '59';
     } else if(seconds === '00' && minutes === '0'){
-        $(".point").css("animation", "inner-circle linear infinite paused");
-        breakManager();
+        $(".point").css("animation-play-state", "paused");
+        isFirstSecond = true;
+        clearInterval(timer);
+        finishedRound();
+        return;
     }
     else {
         seconds = parseInt(seconds, 10);
         seconds--;
     }
-
     (seconds >= 0 && seconds <= 9) ? seconds = '0' + seconds : seconds;
+    $("#countdown").text(minutes + ":" + seconds);
 }
